32253820e91202d521064719d286d244f29b650c
Selomon1 <soya.vandy1950@gmail.com>
2023-07-26 01:53:18 -0700

    Simple Shell Group Project

--- usr_funcs.c
+++ usr_funcs.c
@@ -71,8 +71,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i]; i++)
-		des[i] = src[i];
+		dest[i] = src[i];
 	for (; i < n; i++)
-		dest[] = '\0';
+		dest[i] = '\0';
 	return (dest);
 }
